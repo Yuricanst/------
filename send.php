aec6e5fca14a4d02ae0db5b6bffdf8596c600b39
Yuricanst <duvarov_1987@mail.ru>
2024-06-23 12:41:53 +0300

    update 6

--- send.php
+++ send.php
@@ -1,33 +1,31 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Получение данных из формы
-    $name = $_POST['name'];
-    $phone = $_POST['phone'];
-    $email = $_POST['email'];
-    $budget = $_POST['budget'];
-    $service = $_POST['service'];
-    
-    // Адрес, на который будет отправлено письмо
-    $to = "duvarov_1987@mail.ru";
+// Получение данных из формы
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$budget = $_POST['budget'];
+$service = $_POST['service'];
 
-    // Тема письма
-    $subject = "Новая заявка с формы";
+// Адрес, на который будет отправлено письмо
+$to = "duvarov_1987@mail.ru";
 
-    // Формирование тела письма
-    $body = "От: $name\n";
-    $body .= "Email: $email\n";
-    $body .= "Номер телефона: $phone\n";
-    $body .= "Услуга: $service\n";
-    $body .= "Бюджет: $budget\n";
+// Тема письма
+$subject = "Новая заявка с формы";
 
-    // Отправка письма
-    $mail_sent = mail($to, $subject, $body);
+// Формирование тела письма
+$body = "От: $name\n";
+$body .= "Email: $email\n";
+$body .= "Номер телефона: $phone\n";
+$body .= "Услуга: $service\n";
+$body .= "Бюджет: $budget\n";
 
-    // Проверка успешности отправки
-     if ($mail_sent) {
-        header("Location: thank_you.html");
-    } else {
-        echo "Ошибка отправки. Пожалуйста, попробуйте еще раз.";
-    }
+// Отправка письма
+$mail_sent = mail($to, $subject, $body);
+
+// Проверка успешности отправки
+if ($mail_sent) {
+    header("Location: thank_you.html");
+} else {
+    echo "Ошибка отправки. Пожалуйста, попробуйте еще раз.";
 }
 ?>
